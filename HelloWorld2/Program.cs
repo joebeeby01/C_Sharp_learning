5ad907d04aa323c4da1eac866b7f6a0a7f914c99
Admin <Admin@MIS>
2025-10-20 11:22:04 +0100

    Add project files.

---
+++ HelloWorld2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorld2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Joe";
+            Console.WriteLine("Hello " + name);
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
